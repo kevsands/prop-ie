8fdf3b110bbf475064e89fe5b62f1859e45d1ddf
kevsands <30985181+kevsands@users.noreply.github.com>
2025-04-17 23:38:03 +0100

    Render BuyerDashboard correctly in dashboard.tsx

--- frontend/src/pages/buyer/dashboard.tsx
+++ frontend/src/pages/buyer/dashboard.tsx
@@ -3,14 +3,12 @@ import Layout from '../../components/layout/Layout';
 import BuyerDashboard from '../../components/buyer/BuyerDashboard';
 import ProtectedRoute from '../../components/auth/ProtectedRoute';
 
-const BuyerDashboardPage: React.FC = () => {
-  return (
-    <ProtectedRoute>
-      <Layout>
-        <BuyerDashboard />
-      </Layout>
-    </ProtectedRoute>
-  );
-};
+const BuyerDashboardPage: React.FC = () => (
+  <ProtectedRoute requiredRole="buyer">
+    <Layout>
+      <BuyerDashboard />
+    </Layout>
+  </ProtectedRoute>
+);
 
 export default BuyerDashboardPage;
