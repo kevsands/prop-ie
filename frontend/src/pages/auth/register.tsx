64d720cb4f6729613864adf7b38a700596bcd747
kevsands <30985181+kevsands@users.noreply.github.com>
2025-04-17 23:23:20 +0100

    Correct import paths in register.tsx

--- frontend/src/pages/auth/register.tsx
+++ frontend/src/pages/auth/register.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Layout from '../components/layout/Layout';
-import RegisterForm from '../components/auth/RegisterForm';
+import Layout from '../../components/layout/Layout';
+import RegisterForm from '../../components/auth/RegisterForm';
 
 const RegisterPage: React.FC = () => {
   return (
